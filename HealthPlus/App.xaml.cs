97a3ec223ef616af8dd125252f0dc2c14f59c194
Joao Pedro Gabriel <joaopgabriel80@gmail.com>
2024-04-20 14:48:09 -0300

    Adicionar arquivos de projeto.

---
+++ HealthPlus/App.xaml.cs
@@ -0,0 +1,12 @@
+﻿namespace HealthPlus
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+
+            MainPage = new AppShell();
+        }
+    }
+}
---
+++ HealthPlus/Platforms/MacCatalyst/AppDelegate.cs
@@ -0,0 +1,10 @@
+﻿using Foundation;
+
+namespace HealthPlus
+{
+    [Register("AppDelegate")]
+    public class AppDelegate : MauiUIApplicationDelegate
+    {
+        protected override MauiApp CreateMauiApp() => MauiProgram.CreateMauiApp();
+    }
+}
